78e3b9d03454139b73d2bede3049f1644f3286e8
CorneePSQ <37161255+Cinition@users.noreply.github.com>
2021-05-19 13:50:56 +0200

    Now the custom svg works

--- SVG/icons/index.js
+++
@@ -1,1 +0,0 @@
-export { faPSQ } from "./fa-psq";
--- SVG/index.js
+++
@@ -1,6 +0,0 @@
-import { library, dom } from "../node_modules/@fortawesome/fontawesome-svg-core/index.js";
-import { faPSQ } from "./icons/index.js";
-
-library.add(faPSQ);
-
-dom.watch();
