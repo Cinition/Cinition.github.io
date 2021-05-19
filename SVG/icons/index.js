eece4216168cd1a5d1c46d28027c77cf75d40c22
CorneePSQ <37161255+Cinition@users.noreply.github.com>
2021-05-19 13:10:00 +0200

    Fixed it now

---
+++ SVG/icons/index.js
@@ -0,0 +1,1 @@
+export { faPSQ } from "./fa-psq";
---
+++ SVG/index.js
@@ -0,0 +1,6 @@
+import { library, dom } from "../node_modules/@fortawesome/fontawesome-svg-core";
+import { faPSQ } from "./icons";
+
+library.add(faPSQ);
+
+dom.watch();
